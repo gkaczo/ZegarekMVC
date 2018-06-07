2a2b8661ab6998485920702aeefe58aa1b4da5bf
Grzegorz Kaczorowski <gkaczo@wp.pl>
2018-06-07 09:41:06 +0200

    add

---
+++ ZegarekMVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using ZegarekMVC.Models;
+using KatGodzinaMinuta;
+
+namespace ZegarekMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            DataForm dt = new DataForm();
+            dt.Godzina = DateTime.Now;
+            ViewData.Model = dt;
+            return View();
+        }
+        
+        
+        [HttpPost]
+        public ActionResult PobierzDane(DataForm data)
+        {
+           
+             if (ModelState.IsValid)
+             {
+                GodzinaMinuta gm = new GodzinaMinuta(data.Godzina.Hour, data.Godzina.Minute);
+
+                data.KatStopnie = gm.KatStopnie;
+                data.KatRadiany = gm.KatRadiany;
+                return View("Index", data);
+             }
+           
+            else
+            {
+                return View();
+            }
+        }
+
+  
+    }
+}
---
+++ ZegarekMVC/Models/DataForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ZegarekMVC.Models
+{
+    public class DataForm
+    {
+        [Required]
+        [DataType(DataType.Time)]     
+        [DefaultValue("12")]
+        public DateTime Godzina { get; set; }
+
+
+        public double KatStopnie{get;set;}
+        public double KatRadiany { get; set; }
+
+    }
+}
